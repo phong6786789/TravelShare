b2276252e30a295ecb313875bcd6dde015175052
chau1711996 <chau1711996@gmail.com>
2020-07-21 16:25:55 +0700

    Them Phong lan 1

--- app/src/main/java/com/duan/travelshare/model/ChiTietPhong.java
+++ app/src/main/java/com/duan/travelshare/model/ChiTietPhong.java
@@ -3,19 +3,21 @@ package com.duan.travelshare.model;
 import java.io.Serializable;
 
 public class ChiTietPhong implements Serializable {
-    private String idPhong,gmail,address,idCmt,img;
-    private int price;
+    private String idPhong, tenPhong, giaPhong, diaChiPhong, moTaPhong;
+    FullUser fullUser;
+    HinhPhong imgPhong;
 
     public ChiTietPhong() {
     }
 
-    public ChiTietPhong(String idPhong, String gmail, String address, String idCmt, String img, int price) {
+    public ChiTietPhong(String idPhong, String tenPhong, String giaPhong, String diaChiPhong, String moTaPhong, HinhPhong imgPhong, FullUser fullUser) {
         this.idPhong = idPhong;
-        this.gmail = gmail;
-        this.address = address;
-        this.idCmt = idCmt;
-        this.img = img;
-        this.price = price;
+        this.tenPhong = tenPhong;
+        this.giaPhong = giaPhong;
+        this.diaChiPhong = diaChiPhong;
+        this.moTaPhong = moTaPhong;
+        this.imgPhong = imgPhong;
+        this.fullUser = fullUser;
     }
 
     public String getIdPhong() {
@@ -26,43 +28,51 @@ public class ChiTietPhong implements Serializable {
         this.idPhong = idPhong;
     }
 
-    public String getGmail() {
-        return gmail;
+    public String getTenPhong() {
+        return tenPhong;
     }
 
-    public void setGmail(String gmail) {
-        this.gmail = gmail;
+    public void setTenPhong(String tenPhong) {
+        this.tenPhong = tenPhong;
     }
 
-    public String getAddress() {
-        return address;
+    public String getGiaPhong() {
+        return giaPhong;
     }
 
-    public void setAddress(String address) {
-        this.address = address;
+    public void setGiaPhong(String giaPhong) {
+        this.giaPhong = giaPhong;
     }
 
-    public String getIdCmt() {
-        return idCmt;
+    public String getDiaChiPhong() {
+        return diaChiPhong;
     }
 
-    public void setIdCmt(String idCmt) {
-        this.idCmt = idCmt;
+    public void setDiaChiPhong(String diaChiPhong) {
+        this.diaChiPhong = diaChiPhong;
     }
 
-    public String getImg() {
-        return img;
+    public String getMoTaPhong() {
+        return moTaPhong;
     }
 
-    public void setImg(String img) {
-        this.img = img;
+    public void setMoTaPhong(String moTaPhong) {
+        this.moTaPhong = moTaPhong;
     }
 
-    public int getPrice() {
-        return price;
+    public HinhPhong getImgPhong() {
+        return imgPhong;
     }
 
-    public void setPrice(int price) {
-        this.price = price;
+    public void setImgPhong(HinhPhong imgPhong) {
+        this.imgPhong = imgPhong;
+    }
+
+    public FullUser getFullUser() {
+        return fullUser;
+    }
+
+    public void setFullUser(FullUser fullUser) {
+        this.fullUser = fullUser;
     }
 }
