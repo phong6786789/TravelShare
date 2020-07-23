01fcc196cddb18a960e360e8a02162bfd9e8c8c7
Phong Subi <phong6786789@gmail.com>
2020-07-23 10:52:36 +0700

    Thêm phòng

--- app/src/main/java/com/duan/travelshare/fragment/ManegerPhongThueFragment.java
+++ app/src/main/java/com/duan/travelshare/fragment/ManegerPhongThueFragment.java
@@ -17,59 +17,86 @@ import android.widget.EditText;
 
 import com.duan.travelshare.MainActivity;
 import com.duan.travelshare.R;
+import com.duan.travelshare.firebasedao.PhongDao;
+import com.duan.travelshare.model.ChiTietPhong;
 import com.duan.travelshare.model.FullUser;
 import com.duan.travelshare.model.HinhPhong;
 import com.google.android.gms.common.util.Base64Utils;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class ManegerPhongThueFragment extends Fragment {
+    private PhongDao phongDao;
+    ShowDialog showDialog;
+    FloatingActionButton btnAddPhongThue;
 
-    public ManegerPhongThueFragment(){
+    public ManegerPhongThueFragment() {
 
     }
-    FloatingActionButton btnAddPhongThue;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_maneger_phong_thue, container, false);
+        phongDao = new PhongDao(getActivity());
+        showDialog = new ShowDialog(getActivity());
         btnAddPhongThue = view.findViewById(R.id.btnAddMngPhongThue);
+
         btnAddPhongThue.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                final Dialog dialog = new Dialog(getActivity());
-                dialog.setContentView(R.layout.add_room);
-                dialog.setCancelable(true);
-                Window window = dialog.getWindow();
-                window.setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
-                if (dialog != null && dialog.getWindow() != null) {
-                    dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
-                }
-                final EditText tenPhong, giaPhong, diaChi, moTa;
-                Button btnThem, btnNhapLai;
-                btnThem = dialog.findViewById(R.id.btnTPThem);
-                btnNhapLai = dialog.findViewById(R.id.btnTPNhapLai);
-                tenPhong = dialog.findViewById(R.id.edtTPTieuDe);
-                giaPhong = dialog.findViewById(R.id.edtTPGiaPhong);
-                diaChi = dialog.findViewById(R.id.edtTPDiaChi);
-                moTa = dialog.findViewById(R.id.edtTPMoTaChiTietPhong);
-
-                btnThem.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View view) {
-                        FullUser user = MainActivity.fullUserOne;
-                        HinhPhong hinhPhong = null;
-                        String ten = tenPhong.getText().toString();
-                        String gia = giaPhong.getText().toString();
-                        String dc = diaChi.getText().toString();
-                        String mot = moTa.getText().toString();
-
-                    }
-                });
-
-                dialog.show();
+                addRoom();
             }
         });
         return view;
+    }
+
+    private void addRoom() {
+        final Dialog dialog = new Dialog(getActivity());
+        dialog.setContentView(R.layout.add_room);
+        dialog.setCancelable(true);
+        Window window = dialog.getWindow();
+        window.setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        if (dialog != null && dialog.getWindow() != null) {
+            dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        }
+        final EditText tenPhong, giaPhong, diaChi, moTa;
+        Button btnThem, btnNhapLai;
+        btnThem = dialog.findViewById(R.id.btnTPThem);
+        btnNhapLai = dialog.findViewById(R.id.btnTPNhapLai);
+        tenPhong = dialog.findViewById(R.id.edtTPTieuDe);
+        giaPhong = dialog.findViewById(R.id.edtTPGiaPhong);
+        diaChi = dialog.findViewById(R.id.edtTPDiaChi);
+        moTa = dialog.findViewById(R.id.edtTPMoTaChiTietPhong);
+
+        btnThem.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FullUser user = MainActivity.fullUserOne;
+                HinhPhong hinhPhong = new HinhPhong("1","http");
+                String ten = tenPhong.getText().toString();
+                String gia = giaPhong.getText().toString();
+                String dc = diaChi.getText().toString();
+                String mot = moTa.getText().toString();
+
+                ChiTietPhong chiTietPhong = new ChiTietPhong("", ten, gia, dc, mot, hinhPhong, user);
+                //Thêm phòng
+                phongDao.insertPhong(chiTietPhong);
+                dialog.dismiss();
+            }
+        });
+
+        //Button nhập lại
+        btnNhapLai.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                tenPhong.setText("");
+                giaPhong.setText("");
+                diaChi.setText("");
+                moTa.setText("");
+            }
+        });
+
+        dialog.show();
     }
 }
