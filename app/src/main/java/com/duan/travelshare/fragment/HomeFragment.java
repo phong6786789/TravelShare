dc9e2ed87cb32ef05b9a31ac8feb7afe6bed5b03
nvtuyen2k <vantuyen.nvt1999@gmail.com>
2020-07-30 16:03:13 +0700

    tuyen do du lieu phong home

--- app/src/main/java/com/duan/travelshare/fragment/HomeFragment.java
+++ app/src/main/java/com/duan/travelshare/fragment/HomeFragment.java
@@ -3,22 +3,46 @@ package com.duan.travelshare.fragment;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.duan.travelshare.R;
+import com.duan.travelshare.adapter.PhongAdapter;
+import com.duan.travelshare.firebasedao.PhongDao;
+import com.duan.travelshare.model.ChiTietPhong;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class HomeFragment extends Fragment  {
-
+    RecyclerView rcvPhong;
+    public  static PhongAdapter phongAdapter;
+    List<ChiTietPhong> list;
+    PhongDao phongDao;
     public HomeFragment() {
     }
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
-
+        View view=inflater.inflate(R.layout.fragment_home, container, false);
+        rcvPhong=view.findViewById(R.id.listPhong);
+        list=new ArrayList<>();
+        phongDao=new PhongDao(getActivity());
+        try {
+            list=phongDao.getAllPhong();
+        }catch (Exception ex){
+            ex.printStackTrace();
+        }
+        phongAdapter=new PhongAdapter(list,getActivity());
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getActivity());
+        linearLayoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+        rcvPhong.setLayoutManager(linearLayoutManager);
+        rcvPhong.setAdapter(phongAdapter);
+        phongAdapter.notifyDataSetChanged();
+        return view;
     }
 }
